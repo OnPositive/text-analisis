2fa73f665d1cc95de628afa532b0350988584802
32kda <dmitry.astu@gmail.com>
2014-11-24 19:17:58 +0700

    Dimension parser unit tests

--- com.onpositive.text.analisys.tests/src/com/onpositive/text/analisys/tests/DimensionParserTest.java
+++ com.onpositive.text.analisys.tests/src/com/onpositive/text/analisys/tests/DimensionParserTest.java
@@ -1,17 +1,13 @@
 package com.onpositive.text.analisys.tests;
 
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 
 import com.onpositive.semantic.wordnet.AbstractWordNet;
-import com.onpositive.semantic.wordnet.TextElement;
-import com.onpositive.semantic.wordnet.WordNetProvider;
-import com.onpositive.semantic.wordnet.edit.IWordNetEditInterface;
-import com.onpositive.semantic.wordnet.edit.WordNetPatch;
+import com.onpositive.semantic.wordnet.composite.CompositeWordnet;
 import com.onpositive.text.analysis.IToken;
-import com.onpositive.text.analysis.lexic.PrimitiveTokenizer;
+import com.onpositive.text.analysis.ParserComposition;
+import com.onpositive.text.analysis.lexic.DimensionToken;
+import com.onpositive.text.analysis.lexic.NumericsParser;
 import com.onpositive.text.analysis.lexic.WordFormParser;
 import com.onpositive.text.analysis.lexic.dimension.DimensionParser;
 import com.onpositive.text.analysis.lexic.dimension.UnitGroupParser;
@@ -23,51 +19,77 @@ import junit.framework.TestCase;
 
 public class DimensionParserTest extends TestCase{
 	
+	ParserComposition composition;
 	
-	public void testDimensionParser() {
-		
-		try {
-			URL is = Thread.currentThread().getContextClassLoader().getResource("tst.xml");			
-			InputStreamReader isr = new InputStreamReader(is.openStream(),"UTF-8");
-			WordNetPatch parse = WordNetPatch.parse(isr);
-//			TestCase.assertEquals(14, parse.size());
-			IWordNetEditInterface editable = WordNetProvider.editable(WordNetProvider.getInstance());
-			TextElement wordElement3 = editable.getWordNet().getWordElement("метр");
-			TestCase.assertTrue(wordElement3!=null);
-			TextElement[] possibleContinuations = editable.getWordNet().getPossibleContinuations(wordElement3);
-			parse.execute(editable);
-			
-			doTest(editable);			
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-	}
-
-
-	private void doTest(IWordNetEditInterface editable) {
-		PrimitiveTokenizer pt = new PrimitiveTokenizer();
-		AbstractWordNet wordNet = editable.getWordNet();
+	public DimensionParserTest() {
+		CompositeWordnet wn=new CompositeWordnet();
+		wn.addUrl("/numerics.xml");
+		wn.addUrl("/dimensions.xml");
+		wn.prepare();
+		AbstractWordNet wordNet = wn;
 		WordFormParser wfParser = new WordFormParser(wordNet);
 		ScalarParser scalarParser = new ScalarParser();
 		UnitParser unitParser = new UnitParser(wordNet);
 		UnitGroupParser unitGroupParser = new UnitGroupParser(wordNet);
 		DimensionParser dimParser = new DimensionParser();		
+		NumericsParser numericsParser = new NumericsParser(wn);
+		composition=new ParserComposition(wfParser,scalarParser,numericsParser,unitParser,unitGroupParser,dimParser);
+	}
+	
+	void printTokens(List<IToken> processed) {
+		System.out.println();
+		System.out.println("-----");
 		
-		String str = "Проехал два км со скоростью 10 метров в секунду. Вскопал 2 км^2 земли. Обработал 4 км² асфальта.";		
-		List<IToken> tokens = pt.tokenize(str);		
-		ArrayList<IToken> processed0 = wfParser.process(tokens);
-		ArrayList<IToken> processed1 = scalarParser.process(processed0);
-		ArrayList<IToken> processed2 = unitParser.process(processed1);
-		ArrayList<IToken> processed3 = unitGroupParser.process(processed2);
-		ArrayList<IToken> processed4 = dimParser.process(processed3);
-		
-		
-		for(IToken t : processed4){
+		for(IToken t : processed){
 			System.out.println(t.getStartPosition() + "-" + t.getEndPosition() + " " + TokenTypeResolver.getResolvedType(t) + " " + t.getStringValue());
 		}
-		System.out.println();
 	}
+	
+	void assertTestDimension(double value,List<IToken>tk){
+		boolean found=false;
+		for (IToken z:tk){
+			if (z instanceof DimensionToken){
+				DimensionToken k=(DimensionToken) z;
+				if (k.getValue()==value){
+					found=true;
+				}
+			}
+		}
+		TestCase.assertTrue(found);
+	}
+	
+	private void assertTestDimension(Double[] values, List<IToken> tk) {
+		int ind = 0 ;
+		for (IToken z:tk){
+			if (z instanceof DimensionToken){
+				DimensionToken k=(DimensionToken) z;
+				if (k.getValue()==values[ind]){
+					ind++;
+				}
+			}
+		}
+		TestCase.assertTrue(ind==values.length);
+	}
+	
+	public void testBasic(){
+		String str = "Проехал два км со скоростью 10 метров в секунду";		
+		List<IToken> processed=composition.parse(str);
+		printTokens(processed);
+		assertTestDimension(new Double[]{2.0,10.0}, processed);
+	}
+	
 
+	public void testArea1(){
+		String str = "Вскопал 2 км^2 земли.";		
+		List<IToken> processed=composition.parse(str);
+		printTokens(processed);
+		assertTestDimension(2.0, processed);
+	}
+	
+	public void testArea2(){
+		String str = "Обработал 4 км² асфальта.";		
+		List<IToken> processed=composition.parse(str);
+		printTokens(processed);
+		assertTestDimension(4.0, processed);
+	}
 }
