4445cb8fc15086fbfac675c0f0cd20b25b629a56
Igor Dultsev <yhaskell@live.com>
2015-04-14 15:41:32 +0600

    fixed isModal
    now checking for participles gerunds and infinitives

--- com.onpositive.text.analisys/src/com/onpositive/text/analysis/syntax/ModalLikeVerbsRegistry.java
+++ com.onpositive.text.analisys/src/com/onpositive/text/analysis/syntax/ModalLikeVerbsRegistry.java
@@ -1,8 +1,10 @@
 package com.onpositive.text.analysis.syntax;
 
 import java.util.HashSet;
+
 import com.onpositive.semantic.wordnet.AbstractWordNet;
 import com.onpositive.semantic.wordnet.MeaningElement;
+import com.onpositive.semantic.wordnet.MorphologicalRelation;
 import com.onpositive.semantic.wordnet.TextElement;
 import com.onpositive.semantic.words3.MetaLayer;
 
@@ -33,10 +35,15 @@ public class ModalLikeVerbsRegistry {
 		}
 		int[] allIds = layer.getAllIds();
 		for(int id : allIds){
-			MeaningElement me = wordNet.getConceptInfo(id);
+			MeaningElement me = wordNet.getConceptInfo(id);			
 			TextElement te = me.getParentTextElement();
-			String basicForm = te.getBasicForm();
-			set.add(basicForm);
+			MeaningElement[] concepts = wordNet.getPossibleGrammarForms(te.getBasicForm())[0].getWord().getConcepts();
+			for (MeaningElement c : concepts) {
+				for (MorphologicalRelation r: c.getMorphologicalRelations()) {
+					set.add(r.getWord().getParentTextElement().getBasicForm());					
+				}
+			}
+			set.add(me.getParentTextElement().getBasicForm());
 		}		
 	}
 
