cec84d9c182494a2745618126bcb85596ca23cd0
Yohann Haskell <yhaskell@live.com>
2015-04-27 15:31:22 +0600

    import optimization

--- com.onpositive.text.analisys.tests/src/com/onpositive/text/analisys/tests/ParserTest.java
+++ com.onpositive.text.analisys.tests/src/com/onpositive/text/analisys/tests/ParserTest.java
@@ -6,7 +6,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import sun.reflect.ReflectionFactory.GetReflectionFactoryAction;
 import junit.framework.TestCase;
 
 import com.onpositive.text.analysis.BasicCleaner;
