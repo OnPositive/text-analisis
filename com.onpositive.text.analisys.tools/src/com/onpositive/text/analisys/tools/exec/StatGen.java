d88a95bb78e471e3f337f402f3c8cba6bb7450c8
Yohann Haskell <yhaskell@live.com>
2015-06-08 16:43:00 +0600

    statgen update

--- com.onpositive.text.analisys.tools/src/com/onpositive/text/analisys/tools/exec/StatGen.java
+++ com.onpositive.text.analisys.tools/src/com/onpositive/text/analisys/tools/exec/StatGen.java
@@ -1,18 +1,19 @@
 package com.onpositive.text.analisys.tools.exec;
 
 import java.io.IOException;
-import java.io.PrintStream;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Set;
+
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 
 import com.onpositive.semantic.wordnet.composite.CompositeWordnet;
-import com.onpositive.text.analisys.tools.LexicParserStatGenerator;
+import com.onpositive.text.analisys.tools.WFTConflictStat;
+import com.onpositive.text.analisys.tools.WFTConflictStat.ConflictInfo;
 import com.onpositive.text.analisys.tools.data.HtmlRemover;
-import com.onpositive.text.analisys.tools.exec.ProcessFile.CLibrary;
 import com.onpositive.text.analysis.IToken;
 
 public class StatGen {
@@ -22,14 +23,7 @@ public class StatGen {
 		return new String(encoded, encoding);
 	}
 	
-	public static void main(String [] args) {
-		
- 		if (args.length != 1) {
-			System.err.println("Usage: java -jar statgen.jar FILENAME\n");
-			return;
-		}
- 		PrintStream out = getSysout();
- 		
+	public static CompositeWordnet CreateWordnet() {
 		CompositeWordnet wn=new CompositeWordnet();
 		wn.addUrl("/numerics.xml");
 		wn.addUrl("/dimensions.xml");
@@ -38,7 +32,17 @@ public class StatGen {
 		wn.addUrl("/conjunctions.xml");
 		wn.addUrl("/modalLikeVerbs.xml");
 		wn.prepare();
-		LexicParserStatGenerator sg = new LexicParserStatGenerator(wn);
+		return wn;
+	}
+	
+	public static void main(String [] args) {
+		
+ 		if (args.length != 1) {
+			System.err.println("Usage: java -jar statgen.jar FILENAME\n");
+			return;
+		}
+ 				
+		WFTConflictStat sg = new WFTConflictStat(CreateWordnet());
 		
 		String filename = args[0];
 		
@@ -51,28 +55,36 @@ public class StatGen {
 			System.err.println("IO error on " + filename + ": " + e.getMessage());
 			return;
 		}
-				
 		try {
 			List<IToken> processed = sg.parse(contents);
 			
 			if (processed == null || processed.size() == 0)
-				out.println("Fail on " + filename + ": No tokens produced.");
-			else {
-				out.println("Pass on " + filename + ": Count = " + processed.size());				
-				sg.stats(processed).forEach(stat -> System.out.println(stat.start + " -- " + stat.end + ": " + stat.structure.toString()));				
-			}
+				System.out.println("FAIL " + filename + ": No tokens produced.");
+			else
+				sg.getStatistic(processed).forEach(stat->stat.forEach(x->System.out.println(PrintConflicts(x))));
+			
 		} catch (Exception e) {
-			System.out.println("Fail on " + filename + ": " + e.getMessage());
+			System.out.println("FAIL " + filename + ": " + e.getMessage());
 		}
 	}
-
-	private static PrintStream getSysout() {
-		PrintStream out = null;
+	
+	public static String PrintConflicts(ConflictInfo[] conflicts) {
 		try {
-			out = new PrintStream(System.out, true, "utf-8");
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
+			if (conflicts == null || conflicts.length == 0) return null;
+			
+			JSONArray cs = new JSONArray();
+			
+			for (ConflictInfo c : conflicts) 
+			{
+				JSONObject conflict = new JSONObject();
+				conflict.put("word", c.wft.getBasicForm());
+				conflict.put("partofspeech", c.wft.getPartOfSpeech().toString());
+				conflict.put("tokens", (c.tokenTypes != null ? new JSONArray(c.tokenTypes) : new JSONArray()));				
+				cs.put(conflict);
+			}
+			return cs.toString();
+		} catch (JSONException e) {
+			return null;
 		}
-		return out;
 	}
 }
